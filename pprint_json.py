601f77f46a81ca372aa9f78ec0a23dfd77aaa311
KronCyrcul <ibloko1504@gmail.com>
2018-07-06 21:28:40 +0300

    First commit

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,27 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, encoding="utf-8") as file:
+            bars_data = json.loads(file.read())
+        return bars_data
+    except json.decoder.JSONDecodeError:
+        return None
 
-
+        
 def pretty_print_json(data):
-    pass
+    return json.dumps(data,separators=(",",":"),indent=4,sort_keys=True,ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        filepath = sys.argv[1]
+        json_string = load_data(filepath)
+        if json_string == None:
+            sys.exit("Неверный файл")
+        print(pretty_print_json(json_string))
+    except (IndexError, FileNotFoundError):
+        sys.exit("Введите путь до файла json")
+    
