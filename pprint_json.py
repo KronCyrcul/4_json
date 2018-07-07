2d67425450dc70060586be07abf2bdbf7f36aabe
KronCyrcul <ibloko1504@gmail.com>
2018-07-08 00:48:51 +0300

    changed variable names

--- pprint_json.py
+++ pprint_json.py
@@ -5,23 +5,23 @@ import sys
 def load_data(filepath):
     try:
         with open(filepath, encoding="utf-8") as file:
-            bars_data = json.loads(file.read())
-        return bars_data
+            json_content = json.loads(file.read())
+        return json_content
     except json.decoder.JSONDecodeError:
         return None
 
-        
-def pretty_print_json(data):
-    return json.dumps(data,separators=(",",":"),indent=4,sort_keys=True,ensure_ascii=False)
+
+def pretty_print_json(json_string):
+    return json.dumps(json_string, separators=(",", ":"), indent=4,
+        sort_keys=True, ensure_ascii=False)
 
 
 if __name__ == '__main__':
     try:
         filepath = sys.argv[1]
-        json_string = load_data(filepath)
-        if json_string == None:
+        json_content = load_data(filepath)
+        if json_content is None:
             sys.exit("Неверный файл")
-        print(pretty_print_json(json_string))
+        print(pretty_print_json(json_content))
     except (IndexError, FileNotFoundError):
         sys.exit("Введите путь до файла json")
-    
